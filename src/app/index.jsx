cbf34c57ec8a71979491e7387a8066455dcdc6e0
sarath.c@ishopatoz.com <sarathchembali2002@gmail.com>
2024-04-26 16:46:38 +0530

    Pagination

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, Button } from 'react-native';
 import ExerciseListItem from '../components/ExerciseListItem';
-import {useQuery} from '@tanstack/react-query';
+import {useQuery, useInfiniteQuery} from '@tanstack/react-query';
 import { request,gql } from 'graphql-request';
 import client from '../graphqlClient';
 import { Redirect } from 'expo-router';
 import { useAuth } from '../providers/AuthContext';
 const url = 'https://sitiomarino.us-east-a.ibm.stepzen.net/api/chest-exercises/graphql';
 const exercisesQuery = gql`
-query exercises($muscle: String, $name: String) {
-  exercises(muscle: $muscle, name: $name) {
+query exercises($muscle: String, $name: String, $offset: Int) {
+  exercises(muscle: $muscle, name: $name, offset: $offset) {
     name
     muscle 
     equipment
@@ -18,13 +18,21 @@ query exercises($muscle: String, $name: String) {
 `;
 
 export default function ExercisesScreen() {
-  const {data, isLoading, error} = useQuery({
+  const {data, isLoading, error, fetchNextPage, isFetchingNextPage} = useInfiniteQuery({
     queryKey: ['exercises'],
-    queryFn: () => client.request(exercisesQuery),
+    queryFn: ({ pageParam }) => client.request(exercisesQuery, {offset: pageParam}),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, pages) => pages.length * 10,
     });
 
     const { username } = useAuth();
-
+    
+    const loadMore = () => {
+      if (isFetchingNextPage){
+        return;
+      }
+      fetchNextPage();
+    }
   if(isLoading){
     return <ActivityIndicator />
   }
@@ -35,16 +43,19 @@ export default function ExercisesScreen() {
   if(!username){
     return <Redirect href= {'/auth'} />;
   }
-  
+  const exercises = data?.pages.flatMap((page) => page.exercises);
   return (
     <View style={styles.container}>
       <Text style = {{color: 'white'}}>{username}</Text>
       <FlatList 
-        data = {data?.exercises}
+        data = {exercises}
         contentContainerStyle={{gap: 5}}
         keyExtractor={(item, index) => item.name + index}
         renderItem={({item})=><ExerciseListItem item = {item}/>}
+        onEndReachedThreshold={1}
+        onEndReached={loadMore}
       />
+      
       <StatusBar style = "auto" />
     </View>
   );
